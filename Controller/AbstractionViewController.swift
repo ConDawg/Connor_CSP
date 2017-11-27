6b33055b2ee0ce1e7f905c8f9b2820318b18c06d
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-11-27 13:11:07 -0700

    Created my swipe left and swipe right methods.

--- Controller/AbstractionViewController.swift
+++ Controller/AbstractionViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-public class AbstractionViewController: UIViewController, UIPageViewControllerDataSource
+public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
     
 {
-
+    
     private (set) lazy var orderedAbstractionViews : [UIViewController] =
     {
         return [
@@ -22,36 +22,105 @@ public class AbstractionViewController: UIViewController, UIPageViewControllerDa
             self.newAbstractionViewController(abstractionLevel: "AndGate")
         ]
     }()
-        //Helper method to retrieve the correct ViewController
-        private func newAbstractionViewContoller(abstractionLevel : String) -> UIViewController
-        {
-            return UIStoryboard(name: "Main", bundle : nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
-            
-        }
-        
-        override public func viewDidLoad()
-        {
-            
-        }
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning()
+    //Helper method to retrieve the correct ViewController
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
     {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        return UIStoryboard(name: "Main", bundle : nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
     }
-    */
+    
+    // Do any additional setup after loading the view.
+    
+    
+    public func pageViewController(_ _pageViewController: UIPageViewController, viewControllerBefore viewController:UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+            else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+            else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) ->UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < orderedAbstractionViews.count
+        else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[nextIndex]
+    }
+    
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int
+    {
+        return orderedAbstractionViews.count
+    }
+    
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int
+    {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedAbstractionViews.index(of: firstViewController)
+            else
+            {
+                return 0
+            }
+        return firstViewControllerIndex
+    }
+        
+       override public func didReceiveMemoryWarning()
+        {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destinationViewController.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
+    }
 
-}
