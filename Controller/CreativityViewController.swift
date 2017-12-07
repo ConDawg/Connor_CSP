100c4e6cfd91640c7f7b93ce386ba97a59768aa9
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-12-07 11:57:09 -0700

    added some nice lookin images and set the largePhotoIndexPath var

--- Controller/CreativityViewController.swift
+++ Controller/CreativityViewController.swift
@@ -8,28 +8,59 @@
 
 import UIKit
 
-class CreativityViewController: UIViewController {
+public class CreativityViewController : UICollectionViewController, UICollectionViewDelegateFlowLayout
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInsets UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow : CGFloat = 3
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private lazy var artSelection : [UIImage?] =
+    {
+        return [
+            UIImage(named: "bmo-2"),
+            UIImage(named: "beemoe"),
+            UIImage(named: "cootbmo"),
+            UIImage(named: "bmo-2"),
+            UIImage(named: "cootbmo"),
+            UIImage(named: "bmo-2"),
+            UIImage(named: "beemoe"),
+            UIImage(named: "bmOo"),
+            UIImage(named: "bmOo"),
+        ]
+        // Do any additional setup after loading the view.
+    }()
+    
+    var largePhotoIndexPath: IndexPath?
+    {
+        didSet
+        {
+            var indexPaths = [IndexPath]()
+            if let largePhotoIndexPath = largePhotoIndexPath
+            {
+                indexPaths.append(largePhotoIndexPath)
+            }
+            if let oldValue = oldValue
+            {
+                indexPaths.append(oldValue)
+            }
+            
+            collectionView?.performBatchUpdates(
+                {
+                    self.collectionView?.reloadItems(at: indexPaths)
+            })
+            {
+                completed in
+                
+                if let largePhotoIndexPath = self.largePhotoIndexPath
+                {
+                    self.collectionView?.scrollToItem(at:largePhotoIndexPath,
+                                                      at: .centeredVertically,
+                                                      animated: true)
+                }
+                }
+            }
+            
+        }
     }
-    */
 
-}
