6019ab7a70ffd8824dbdfa0c8c55dc744071a3ab
ConDawg <31518714+ConDawg@users.noreply.github.com>
2018-01-16 12:17:52 -0700

    added var bucketItem, randomEmoji, and updateCellView

--- Controller/DataView/BucketItemCell.swift
+++ Controller/DataView/BucketItemCell.swift
@@ -19,11 +19,54 @@ class BucketItemCell: UITableViewCell
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    var bucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
     }
+    
+    private func randomEmoji() -> String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1FC5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+    private func updateCellView()
+    {
+        if (bucketItem != nil)
+        {
+            bucketItemSignature.text = bucketItem.itemAuthor
+            bucketItemText.text = bucketItem.itemContents
+        }
+        else
+        {
+            bucketItemSignature.text = "author goes here"
+            bucketItemText.text = "text goes here"
+        }
+        
+        bucketItemSymbol.text = randomEmojiSymbol()
+    }
+    
+    public override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+    
+    public override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+    }
+
 
 }
